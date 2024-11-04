51aaffc1dcebd692edbd01fc255031edeb3863f0
Divyanshu Grover <142349410+DivyanshuGrover29@users.noreply.github.com>
2024-11-05 02:03:33 +0530

    Update LatestJobs.jsx

--- frontend/src/components/shared/LatestJobs.jsx
+++ frontend/src/components/shared/LatestJobs.jsx
@@ -10,7 +10,7 @@ const LatestJobs = () => {
    
     return (
         <div className='max-w-7xl mx-auto my-20 px-4'>
-            <h1 className='text-4xl font-bold  mb-5'><span className='text-[#6A38C2]'>Latest & Top </span> Job Openings</h1>
+            <h1 className='text-4xl font-bold  mb-5'><span className='text-[#4a38c2]'>Latest & Top </span> Job Openings</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
                     allJobs.length <= 0 ? <span>No Job Available</span> : allJobs?.slice(0,6).map((job) => <LatestJobCards key={job._id} job={job}/>)
